b868456bd8a24563417783d8a2eee3ac22b06930
Дима Будянский <budyanskiy1234@gmail.com>
2025-11-16 15:08:53 +0200

    Add project files.

---
+++ PolynomeSolverConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PolynomeSolverConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
