440458a906e442066f77a5579a136043385b2d6f
Дима Будянский <budyanskiy1234@gmail.com>
2025-11-18 19:00:32 +0200

    final minor changes

---
+++ PolynomLib/DataManager/ISerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolynomialLib.DataManager
+{
+    internal interface ISerializer<T>
+    {
+        public T Read(string path);
+        public void Write(T obj, string path);
+    }
+}
